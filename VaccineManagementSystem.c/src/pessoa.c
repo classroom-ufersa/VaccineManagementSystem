ecfe885f8985e60eccad6fd57560a7060d4326c8
Vladimyr Guedes <vladguedes@hotmail.com>
2023-04-28 16:49:14 -0300

    Criando as funções de criar e adicionar pessoa.

--- VaccineManagementSystem.c/src/pessoa.c
+++ VaccineManagementSystem.c/src/pessoa.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "vacina.h"
 
-
-typedef struct pessoa{
+struct pessoa{
     char nome[100];
     int idade;
     int documento;
-    Vacina* cartao_vac;
-}Pessoa;
+    //Cartao de vacina
+};
 
-//teste
+Pessoa* cria_pessoa() {
+    return(NULL);
+}
+
+Pessoa* add_pessoa(Pessoa* p) {
+    Pessoa* nova = (Pessoa*) malloc(sizeof(Pessoa));
+
+    printf("Informe o nome da pessoa: ");
+    scanf(" %[^\n]s", &p->nome);
+    printf("Informe a idade da pessoa: ");
+    scanf("%d", &p->idade);
+    printf("Informe o documento da pessoa: ");
+    scanf("%d", &p->documento);
+
+    return(nova);
+}
+
+void listar_pessoas(Pessoa* p) {
+    Pessoa* listar;
+
+    for(listar = p; listar != NULL; listar = listar->)
+}
