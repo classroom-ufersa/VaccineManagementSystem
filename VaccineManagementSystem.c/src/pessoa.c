69bcf58b26631d16b1954329584a6fcb1ef0f2c7
Abner Torres <abnergama2512@gmail.com>
2023-04-28 08:51:08 -0300

    ft: small changes

---
+++ VaccineManagementSystem.c/src/pessoa.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct pessoa{
+    char nome[100];
+    int idade;
+    int documento;
+    //Cartao de vacina
+};
---
+++ VaccineManagementSystem.c/src/vacina.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct vacina{
+    char nome[50];
+    int lote;
+    char data_fab[20];
+    char data_val[20];
+};
+
