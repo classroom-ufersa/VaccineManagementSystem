39b33d7f8348e7d4ea324d0f30024e6648afe154
SrVlady <vladguedes@hotmail.com>
2023-05-07 10:49:53 -0300

    ...

--- VaccineManagementSystem.c/src/main.c
+++ VaccineManagementSystem.c/src/main.c
@@ -14,7 +14,7 @@ int main(){
     printf(" _______________________________________________ \n|\tBem vindo ao Gerenciador de Vacinas\t|\n|\tDesenvolvido por:\t\t\t|\n|\t>Abner Gama Torres\t\t\t|\n|\t>Vladimyr de Oliveira Guedes\t\t|\n|_______________________________________________|\n");
     while(Opcao != 13){
         printf("Digite a opção desejada:\n");
-        printf("1-Adicionar vacina\n2-Remover vacina\n3-Listar vacinas\n4-Buscar vacinas\n5-Aplicação de vacina em uma pessoa\n6-Editar vacina cadastrada\n7-Consultar quantidade de pessoas que aplicaram determinada vacina\n8-Adicionar pessoa\n9-Listar pessoas\n10-Sair do programa\n");
+        printf("1-Adicionar Vacina\n2-Remover Vacina\n3-Listar Vacinas\n4-Buscar Vacinas\n5-Aplicação de Vacina em Uma Pessoa\n6-Editar Vacina Cadastrada\n7-Consultar Quantidade de Pessoas que Aplicaram Determinada Vacina\n8-Adicionar Pessoa\n9-Listar Pessoas\n10-Remover Pessoa\n11-Editar Pessoa\n12-Buscar Pessoa\n13-Sair do Pograma\n");
         scanf("%i", &Opcao);
         system("clear");
         switch(Opcao){
@@ -48,7 +48,6 @@ int main(){
             printf("\tAdicionar Pessoa\t\n");
             pessoa = cria_pessoa();
             pessoa = add_pessoa();
-
             //função de adicionar pessoa
             break;
             case 9:
